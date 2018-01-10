e1d3d708c7c7f02a7fcf8c6f4a1018ba67ddc5e2
Ubuntu <ubuntu@ip-172-31-35-8.us-east-2.compute.internal>
2018-01-10 12:48:22 +0000

    terraform

---
+++ public.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "web" {
+    name = "vpc_web"
+    description = "Allow incoming HTTP connections."
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+vpc_id = "${aws_vpc.yogesh-demo.id}"
+
+    tags {
+        Name = "WebServerSG"
+    }
+}
+resource "aws_instance" "web-1" {
+    ami = "ami-82f4dae7"
+    availability_zone = "us-east-2b"
+    instance_type = "t2.micro"
+    key_name = "${var.aws_key_name}"
+    vpc_security_group_ids = ["${aws_security_group.web.id}"]
+    subnet_id = "${aws_subnet.us-east-1a-public.id}"
+    associate_public_ip_address = true
+    source_dest_check = false
+
+
+    tags {
+        Name = "Web Server 1"
+    }
+}
---
+++ vpc.tf
@@ -0,0 +1,44 @@
+resource "aws_vpc" "yogesh-demo" {
+    cidr_block = "${var.vpc_cidr}"
+   # enable_dns_hostnames = true
+    tags {
+        Name = "terraform-aws-vpc"
+    }
+}
+
+resource "aws_internet_gateway" "default" {
+    vpc_id = "${aws_vpc.yogesh-demo.id}"
+}
+
+#Public Subnet
+resource "aws_subnet" "us-east-1a-public" {
+    vpc_id = "${aws_vpc.yogesh-demo.id}"
+
+    cidr_block = "${var.public_subnet_cidr}"
+    availability_zone = "us-east-2b"
+
+    tags {
+        Name = "Public Subnet"
+    }
+}
+
+#route table
+resource "aws_route_table" "us-east-1a-public" {
+    vpc_id = "${aws_vpc.yogesh-demo.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.default.id}"
+    }
+
+    tags {
+        Name = "Public Subnet"
+    }
+}
+
+resource "aws_route_table_association" "us-east-1a-public" {
+    subnet_id = "${aws_subnet.us-east-1a-public.id}"
+    route_table_id = "${aws_route_table.us-east-1a-public.id}"
+}
+
+
